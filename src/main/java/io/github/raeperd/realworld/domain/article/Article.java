ddb5995d3e54f1aec35e6eff09b9ddfc730c31fa
henry <cksgod16@gmail.com>
2023-07-03 22:36:06 +0900

    analysis.0 : Identify refactoring areas in article entity
    - 현재 Article 엔티티는 SRP를 위반 하는 코드로 리팩토링이 필요.
    - Boolean flag 에 대한 처리 구현하기.
    - 매개변수 변경

--- src/main/java/io/github/raeperd/realworld/domain/article/Article.java
+++ src/main/java/io/github/raeperd/realworld/domain/article/Article.java
@@ -18,10 +18,11 @@ import static javax.persistence.CascadeType.REMOVE;
 import static javax.persistence.FetchType.EAGER;
 import static javax.persistence.GenerationType.IDENTITY;
 
+
 @Table(name = "articles")
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article { // JPA 결합되어 있는 엔티티 클래스
 
     @GeneratedValue(strategy = IDENTITY)
     @Id
@@ -31,7 +32,7 @@ public class Article {
     @ManyToOne(fetch = EAGER)
     private User author;
 
-    @Embedded
+    @Embedded // ArticleContents가 Article에 포함된다. 단일 데이터베이스 테이블에 매핑된다.
     private ArticleContents contents;
 
     @Column(name = "created_at")
@@ -45,15 +46,17 @@ public class Article {
     @JoinTable(name = "article_favorites",
             joinColumns = @JoinColumn(name = "article_id", referencedColumnName = "id", nullable = false),
             inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false))
+    // TODO("다대다 관계를 1:다 다:1 로 해소)
     @ManyToMany(fetch = EAGER, cascade = PERSIST)
     private Set<User> userFavorited = new HashSet<>();
 
     @OneToMany(mappedBy = "article", cascade = {PERSIST, REMOVE})
     private Set<Comment> comments = new HashSet<>();
 
-    @Transient
-    private boolean favorited = false;
+    @Transient // 데이터베이스에 매핑되지 않는다.
+    private boolean favorited = false; // 안티 패턴
 
+    // 생성자
     public Article(User author, ArticleContents contents) {
         this.author = author;
         this.contents = contents;
@@ -62,9 +65,10 @@ public class Article {
     protected Article() {
     }
 
-    public Article afterUserFavoritesArticle(User user) {
-        userFavorited.add(user);
-        return updateFavoriteByUser(user);
+
+    public Article afterUserFavoritesArticle(User user) { // 사용자가 게시글을 좋아요 했을 때 호출
+        userFavorited.add(user); // 사용자가 게시글을 좋아요 했음을 추가
+        return updateFavoriteByUser(user); // 게시글의 좋아요 상태를 업데이트
     }
 
     public Article afterUserUnFavoritesArticle(User user) {
@@ -72,13 +76,20 @@ public class Article {
         return updateFavoriteByUser(user);
     }
 
-    public Comment addComment(User author, String body) {
+    // updateFavoriteByUser(User user): 사용자에 따른 Article의 "favorite" 상태를 업데이트하는 메서드입니다. userFavorited 세트가 사용자를 포함하고 있는지 확인하고, 이에 따라 favorited 필드의 값을 업데이트합니다.
+    public Article updateFavoriteByUser(User user) { // 사용자가 favorite을 누르면 article에서 favorite을 추가한다.
+        favorited = userFavorited.contains(user); // boolean
+        return this; // 현재 인스턴스를 반환한다.
+    }
+
+
+    public Comment addComment(User author, String body) { // 매개변수 이름 변경
         final var commentToAdd = new Comment(this, author, body);
         comments.add(commentToAdd);
         return commentToAdd;
     }
 
-    public void removeCommentByUser(User user, long commentId) {
+    public void removeCommentByUser(User user, long commentId) { // 책임 분리 필요
         final var commentsToDelete = comments.stream()
                 .filter(comment -> comment.getId().equals(commentId))
                 .findFirst()
@@ -93,11 +104,7 @@ public class Article {
         contents.updateArticleContentsIfPresent(updateRequest);
     }
 
-    public Article updateFavoriteByUser(User user) {
-        favorited = userFavorited.contains(user);
-        return this;
-    }
-
+    //** boilerplate code
     public User getAuthor() {
         return author;
     }
@@ -126,6 +133,7 @@ public class Article {
         return comments;
     }
 
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -138,4 +146,5 @@ public class Article {
     public int hashCode() {
         return Objects.hash(author, contents.getTitle());
     }
+    //** boilerplate code
 }
